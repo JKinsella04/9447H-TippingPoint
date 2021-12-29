f5f2f29a8a2262bd4be05a0d289d7654423a90d4
Jack <jkinsella@psaschool.org>
2021-12-28 21:57:21 -0500

    fixed ports

--- src/globals.cpp
+++ src/globals.cpp
@@ -7,26 +7,26 @@ pros::Controller master(CONTROLLER_MASTER);
 
 // Motor Definitions //
 // Lift Definition
-pros::Motor arm(17, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS);
+pros::Motor arm(1, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS);
 
 // Intake Definition
-pros::Motor intake(3, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS);
+pros::Motor intake(9, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS);
 
 //Drive Base Definitions
 pros::Motor LF(20, MOTOR_GEARSET_18, 1, MOTOR_ENCODER_COUNTS), 
-            LM(10, MOTOR_GEARSET_18, 1, MOTOR_ENCODER_COUNTS),
+            LM(8, MOTOR_GEARSET_18, 1, MOTOR_ENCODER_COUNTS),
             LB(10, MOTOR_GEARSET_18, 1, MOTOR_ENCODER_COUNTS),
             RF(11, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS),
-            RM(11, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS),
-            RB(1, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS);
+            RM(3, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS),
+            RB(2, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_COUNTS);
 
 // Inertial sensor Definitions //
-pros::Imu L_Imu(16), R_Imu(9);
+pros::Imu L_Imu(18), R_Imu(13);
 
 // Pneumatic Definitions //
 pros::ADIDigitalOut frontClamp ('H'),
-                    backArm('A'),
-                    backClamp('B');
+                    backArm('D'),
+                    backClamp('F');
 
 // GPS Definition // 
 pros::Gps gps(5, 0, -0.3048); 
