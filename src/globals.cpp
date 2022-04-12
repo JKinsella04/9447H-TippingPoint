edba38715652f5ea020158d79f83327818089b29
Jack <jkinsella@psaschool.org>
2022-04-12 11:51:17 -0400

    reverse intake

--- src/globals.cpp
+++ src/globals.cpp
@@ -8,7 +8,7 @@ pros::Controller master(CONTROLLER_MASTER);
 pros::Motor arm(13, MOTOR_GEARSET_18, 1, MOTOR_ENCODER_COUNTS);
 
 // Intake Definition
-pros::Motor intake(15, MOTOR_GEARSET_6, 1, MOTOR_ENCODER_COUNTS);
+pros::Motor intake(15, MOTOR_GEARSET_6, 0, MOTOR_ENCODER_COUNTS);
 
 //Drive Base Definitions
 pros::Motor LF(6, MOTOR_GEARSET_6, 1, MOTOR_ENCODER_COUNTS), 
